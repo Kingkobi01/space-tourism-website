0fa9acbe191d54bb5404557ab3b90454eaa74739
Kwabena Boakye <kwaboakye93@gmail.com>
2024-04-13 20:47:31 +0000

    Add postcss.config.js and tailwind.config.js files

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0B0D17",
+        secondary: "#D0D6F9"
+      },
+      fontFamily: {
+        serif: ["Bellefair", "sans-serif"],
+        condensed: ["Barlow Condensed", "sans-serif"],
+        sans: ["Barlow", "sans-serif"],
+      },
+      fontSize: {
+        xs: "0.875rem",
+        sm: "1rem",
+        md: "1.125rem",
+        lg: "1.625rem",
+        xl: "1.75rem",
+        "2xl": "2rem",
+        "3xl": "3.5rem",
+        "4xl": "6.25rem",
+        "5xl": "9.375rem",
+      }
+    },
+  },
+  plugins: [],
+}
+
