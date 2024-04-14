9d83be57432aba583aa6f43dd3ed4812a6729213
Kwabena Boakye <kwaboakye93@gmail.com>
2024-04-14 19:35:10 +0000

    Add Explore component

---
+++ src/components/Explore.tsx
@@ -0,0 +1,11 @@
+const Explore = () => {
+  return (
+    <div className="place-items-center grid hover:bg-secondary/20 rounded-full w-[230px] md:w-[400px] lg:w-[450px] duration-300 aspect-square">
+      <div className="place-items-center grid bg-white rounded-full w-[127px] md:w-[242px] lg:w-[274px] text-primary aspect-square">
+        <h4>Explore</h4>
+      </div>
+    </div>
+  );
+};
+
+export default Explore;
