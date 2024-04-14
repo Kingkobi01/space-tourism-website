e16a4d724092866c926dccf1dbda0fd6ae35f8fd
Kwabena Boakye <kwaboakye93@gmail.com>
2024-04-14 20:59:54 +0000

    Add Crew and Technology page components

---
+++ src/pages/Crew.tsx
@@ -0,0 +1,7 @@
+const Crew = () => {
+  return (
+    <div className="lg:before:bg-[url('./assets/crew/background-crew-desktop.jpg')] before:top-0 before:left-0 z-100 before:-z-30 before:absolute flex lg:flex-row flex-col lg:justify-between items-center gap-[6rem] lg:gap-auto md:before:bg-[url('./assets/crew/background-crew-tablet.jpg')] before:bg-[url('./assets/crew/background-crew-mobile.jpg')] before:bg-cover mx-auto mt-[3rem] md:mt-[1rem] md:ml-auto w-[90%] md:w-[70%] lg:w-[80%] before:w-[100vw] before:h-full lg:min-h-screen text-center lg:text-start"></div>
+  );
+};
+
+export default Crew;
---
+++ src/pages/Technology.tsx
@@ -0,0 +1,7 @@
+const Technology = () => {
+  return (
+    <div className="lg:before:bg-[url('./assets/technology/background-technology-desktop.jpg')] before:top-0 before:left-0 z-100 before:-z-30 before:absolute flex lg:flex-row flex-col lg:justify-between items-center gap-[6rem] lg:gap-auto md:before:bg-[url('./assets/technology/background-technology-tablet.jpg')] before:bg-[url('./assets/technology/background-technology-mobile.jpg')] before:bg-cover mx-auto mt-[3rem] md:mt-[1rem] md:ml-auto w-[90%] md:w-[70%] lg:w-[80%] before:w-[100vw] before:h-full lg:min-h-screen text-center lg:text-start"></div>
+  );
+};
+
+export default Technology;
