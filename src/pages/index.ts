2e5f886aef54ee32fece9737b5d281a927857f5e
Kwabena Boakye <kwaboakye93@gmail.com>
2024-04-14 21:00:08 +0000

    Add new page components: Crew and Technology

---
+++ src/pages/index.ts
@@ -0,0 +1,6 @@
+import Crew from "./Crew";
+import Destination from "./Destination";
+import Home from "./Home";
+import Technology from "./Technology";
+
+export { Crew, Destination, Home, Technology };
